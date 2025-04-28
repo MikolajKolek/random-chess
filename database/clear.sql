844b1480a61d2048986ca6743d4e020933f37741
Mikolaj Kolek <kolek.mikolaj@gmail.com>
2025-04-28 20:19:53 +0200

    Drop functions in clear.sql

--- database/clear.sql
+++ database/clear.sql
@@ -1,2 +1,3 @@
 DROP VIEW IF EXISTS game_openings;
 DROP TABLE IF EXISTS users, service_games, service_accounts, game_services, games, pgn_games, openings;
+DROP FUNCTION IF EXISTS add_default_service_to_user(), prevent_default_service_modification(), prevent_default_service_deletion();
