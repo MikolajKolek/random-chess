af789232efb0d28e02e76dfbdf6402bacad08961
Mikolaj Kolek <kolek.mikolaj@gmail.com>
2025-04-28 20:39:21 +0200

    Add example data to create.sql

--- database/create.sql
+++ database/create.sql
@@ -153,24 +153,51 @@ CREATE OR REPLACE TRIGGER service_accounts_delete_prevent_for_default_service
 EXECUTE FUNCTION prevent_default_service_deletion();
 
 
-INSERT INTO game_services(id, name) VALUES
-    (1, 'chess.com'),
-    (2, 'lichess.org');
 
--- INSERT INTO service_accounts("service_id", "user_id_in_service", "is_bot", "display_name") VALUES
---      ()
+-- Przykładowe dane:
+INSERT INTO game_services(name) VALUES
+    ('chess.com'),
+    ('lichess.org');
 
--- INSERT INTO service_games("moves", "date", service_id, "game_id_in_service", white_player, black_player) VALUES
---     (
---         '1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd8 { B01 Scandinavian Defense: Valencian Variation } 4. d4 Nf6 5. Nf3 g6 6. Bc4 Bg7 7. O-O O-O 8. Re1 Nbd7 9. Bg5 Nb6 10. Bd3 c6 11. Ne2 Nbd5 12. c3 Nb6 13. h3 Re8 14. Ng3 Be6 15. Qd2 Qd7 16. Bh6 Rad8 17. Ng5 Bxh6 18. N3e4 Nxe4 19. Bxe4 Bd5 20. Bd3 f6 21. h4 fxg5 22. hxg5 Bg7 23. a4 e5 24. a5 Nc4 25. Qe2 exd4 26. Bxc4 Rxe2 27. Bxd5+ Qxd5 28. Rxe2 dxc3 { White resigns. } 0-1',
---         '2025-04-24T16:02:54Z',
---         2,
---         'zGsFNtCE',
---         'TLTLTLTLTLTLTLTLTIT',
---         'chess-art-us'
---     );
+INSERT INTO users(email, password_hash) VALUES
+    ('test@[1.1.1.1]', '1234'),
+    ('email.test@gmail.com', '0000'),
+    ('test2.user@interia.pl', '1111');
 
-/*INSERT INTO users(email, password_hash, elo) VALUES ('test@[1.1.1.1]', '123', 0)
-INSERT INTO pgn_games("id", "moves", "date", "owner_id", "black_player_name", "white_player_name", "metadata") VALUES
-                                             (1, '69', '2003-04-12 04:05:06', 1, 'a', 'b', '{}')
-*/
+UPDATE service_accounts SET display_name = 'test user' WHERE user_id = 1;
+
+INSERT INTO service_accounts("user_id", "service_id", "user_id_in_service", "is_bot", "display_name") VALUES
+    (2, 2, 'test_ccom_id', FALSE, 'test_chesscom'),
+    (3, 2, 'test2_ccom_id', FALSE, 'test2_chesscom'),
+    (3, 3, 'test2_lc_id', FALSE, 'test2_lichess'),
+    (NULL, 3, 'chess_com_user', FALSE, 'chess_com_user_not_in_service'),
+    (NULL, 2, 'chess_com_bot', TRUE, 'chess_com_bot'),
+    (NULL, 1, 'internal_bot', TRUE, 'bot_1');
+
+INSERT INTO service_games("moves", "date", "metadata", "service_id", "game_id_in_service", white_player, black_player) VALUES
+    (
+        '1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd8 { B01 Scandinavian Defense: Valencian Variation } 4. d4 Nf6 5. Nf3 g6 6. Bc4 Bg7 7. O-O O-O 8. Re1 Nbd7 9. Bg5 Nb6 10. Bd3 c6 11. Ne2 Nbd5 12. c3 Nb6 13. h3 Re8 14. Ng3 Be6 15. Qd2 Qd7 16. Bh6 Rad8 17. Ng5 Bxh6 18. N3e4 Nxe4 19. Bxe4 Bd5 20. Bd3 f6 21. h4 fxg5 22. hxg5 Bg7 23. a4 e5 24. a5 Nc4 25. Qe2 exd4 26. Bxc4 Rxe2 27. Bxd5+ Qxd5 28. Rxe2 dxc3 { White resigns. } 0-1',
+        '2025-04-24T16:02:54Z',
+        '{"TimeControl": "30+3"}',
+        3,
+        'zGsFNtCE',
+        'test2_lc_id',
+        'chess_com_user'
+    );
+
+INSERT INTO pgn_games("moves", "date","metadata", "owner_id", "black_player_name", "white_player_name") VALUES
+    (
+        '1. d4 Nf6 2. c4 e6 3. Nf3 d5 4. g3 c6 5. Qc2 Bb4+ 6. Nbd2 O-O 7. Bg2 b6 8. O-O Ba6 9. b3 Nbd7 10. Bb2 Rc8 11. Rfd1 c5 12. a3 Bxd2 13. Nxd2 cxd4 14. Bxd4 e5 15. Bb2 dxc4 16. bxc4 Qe7 17. Qa4 Nc5 18. Qb4 Rfe8 19. Nf1 Bb7 20. Bh3 Rcd8 21. Ne3 Qc7 22. f3 a5 23. Rxd8 Rxd8 24. Qc3 Re8 25. Qc2 h6 26. Rd1 Bc6 27. Rd2 Ba4 28. Qc3 Bc6 29. Nd5 Bxd5 30. cxd5 Qd6 31. e4 Na4 32. Qc6 Qc5+ 33. Qxc5 Nxc5 34. d6 Nfd7 35. Rc2 f6 36. Bf1 Rd8 37. Bb5 Nf8 38. Rd2 Nfe6 39. a4 Nb3 40. Rd5 Nbd4 41. Bc4 Nxf3+ 42. Kf2 Nfg5 43. Ke3 Kf8 44. Ba3 Nc5 45. Bxc5 bxc5 46. Rxc5 Rxd6 47. h4 Ne6 48. Rxa5 Nc7 49. Rc5 Na6 50. Rc8+ Ke7 51. a5 Nb4 52. Rg8 g5 53. h5 Nc2+ 54. Kf2 Nb4 55. Rg7+ Kf8 56. Rb7 Nd3+ 57. Bxd3 Rxd3 58. a6 Rd2+ 59. Ke3 Ra2 60. a7 Ra3+ 61. Ke2 1-0',
+        '2025-04-28T19:39:01Z',
+        '{"Event": "World Championships 2025", "Site": "Krakow, Poland"}',
+        1,
+        'aaa',
+        'bbb'
+    );
+
+INSERT INTO openings("eco", "name", "epd") VALUES
+    (
+        'D11',
+        'Slav Defense: Modern Line',
+        'rnbqkbnr/pp2pppp/2p5/3p4/2PP4/5N2/PP2PPPP/RNBQKB1R b KQkq -'
+    );
