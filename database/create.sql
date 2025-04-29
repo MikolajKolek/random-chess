06c720c7792ca832b02f812fa3fa16d6590dc591
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-04-29 20:48:27 +0200

    Remove table games

--- database/create.sql
+++ database/create.sql
@@ -50,9 +50,27 @@ CREATE TABLE "service_accounts"
     )
 );
 
+-- Tworzymy tabele service_games i pgn_games.
+-- To rozwiązanie Niektóre z ich kolumn się pokrywają.
+-- Ze względu na ograniczenia mechanizmów polimorficzności w PostgreSQL zdecydowaliśmy,
+-- że jest to najlepsze rozwiązanie. Opis innych rozważanych rozwiązań dołączamy
+-- do zgłoszenia projektu.
+--
+-- "id" w tabelach service_games i pgn_games są unikalne tylko w obrębie danej tabeli.
+-- W widoku "games" jako klucza należy używać pary ("id", "kind").
+
+-- TODO: napisać funkcję generującą listę pozycji w formacie EPD dla gry
+-- i dodać to wspólne pole do pgn_games i serial_games:
+-- "epd_positions"     VARCHAR[]       GENERATED ALWAYS AS ()
+
 CREATE TABLE "service_games"
 (
     "id"                 SERIAL         PRIMARY KEY,
+    -- kolumny wspólne dla "service_games" i "pgn_games"
+    "moves"             VARCHAR         NOT NULL,
+    "date"              TIMESTAMP       NULL,
+    "metadata"          JSONB           NULL,
+    -- kolumny występujące tylko w "service_games"
     "game_id_in_service" VARCHAR        NOT NULL,
     "service_id"         INT            NOT NULL    REFERENCES "game_services" ("id"),
     "white_player"       VARCHAR        NOT NULL,
@@ -67,23 +85,21 @@ CREATE TABLE "service_games"
 CREATE TABLE "pgn_games"
 (
     "id"                SERIAL          PRIMARY KEY,
+    -- kolumny wspólne dla "service_games" i "pgn_games"
+    "moves"             VARCHAR         NOT NULL,
+    "date"              TIMESTAMP       NULL,
+    "metadata"          JSONB           NULL,
+    -- kolumny występujące tylko w "pgn_games"
     "owner_id"          INT             NOT NULL    REFERENCES "users" ("id") ON DELETE CASCADE,
     "black_player_name" VARCHAR         NOT NULL,
     "white_player_name" VARCHAR         NOT NULL
 );
 
-CREATE TABLE "games"
-(
-    "id"                SERIAL          PRIMARY KEY,
-    "moves"             VARCHAR         NOT NULL,
-    "date"              TIMESTAMP       NULL,
-    "metadata"          JSONB           NULL,
-    -- TODO: napisać funkcję generującą listę pozycji w formacie EPD dla gry
-    -- "epd_positions"     VARCHAR[]       GENERATED ALWAYS AS ()
-    "service_game_id"   INT             UNIQUE NOT NULL     REFERENCES "service_games" ("id"),
-    "pgn_game_id"       INT             UNIQUE NOT NULL     REFERENCES "pgn_games" ("id"),
-    --
-    CHECK (("service_game_id" IS NOT NULL)::int + ("pgn_game_id" IS NOT NULL)::int = 1)
+-- Wartości "id" mogą się powtarzać, ale już pary ("id", "kind") są unikatowe
+CREATE VIEW "games" AS (
+    SELECT "id", 'service' as "kind", "moves", "date", "metadata" FROM service_games
+    UNION ALL
+    SELECT "id", 'pgn' as "kind", "moves", "date", "metadata" FROM pgn_games
 );
 
 -- TODO: stworzyć view który na podstawie tabeli openings i epd_positions w games przypisuje każdej grze opening
@@ -177,26 +193,26 @@ INSERT INTO service_accounts("user_id", "service_id", "user_id_in_service", "is_
     (NULL, 2, 'chess_com_bot', TRUE, 'chess_com_bot'),
     (NULL, 1, 'internal_bot', TRUE, 'bot_1');
 
--- INSERT INTO service_games("moves", "date", "metadata", "service_id", "game_id_in_service", white_player, black_player) VALUES
---     (
---         '1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd8 { B01 Scandinavian Defense: Valencian Variation } 4. d4 Nf6 5. Nf3 g6 6. Bc4 Bg7 7. O-O O-O 8. Re1 Nbd7 9. Bg5 Nb6 10. Bd3 c6 11. Ne2 Nbd5 12. c3 Nb6 13. h3 Re8 14. Ng3 Be6 15. Qd2 Qd7 16. Bh6 Rad8 17. Ng5 Bxh6 18. N3e4 Nxe4 19. Bxe4 Bd5 20. Bd3 f6 21. h4 fxg5 22. hxg5 Bg7 23. a4 e5 24. a5 Nc4 25. Qe2 exd4 26. Bxc4 Rxe2 27. Bxd5+ Qxd5 28. Rxe2 dxc3 { White resigns. } 0-1',
---         '2025-04-24T16:02:54Z',
---         '{"TimeControl": "30+3"}',
---         3,
---         'zGsFNtCE',
---         'test2_lc_id',
---         'chess_com_user'
---     );
+INSERT INTO service_games("moves", "date", "metadata", "service_id", "game_id_in_service", white_player, black_player) VALUES
+    (
+        '1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd8 { B01 Scandinavian Defense: Valencian Variation } 4. d4 Nf6 5. Nf3 g6 6. Bc4 Bg7 7. O-O O-O 8. Re1 Nbd7 9. Bg5 Nb6 10. Bd3 c6 11. Ne2 Nbd5 12. c3 Nb6 13. h3 Re8 14. Ng3 Be6 15. Qd2 Qd7 16. Bh6 Rad8 17. Ng5 Bxh6 18. N3e4 Nxe4 19. Bxe4 Bd5 20. Bd3 f6 21. h4 fxg5 22. hxg5 Bg7 23. a4 e5 24. a5 Nc4 25. Qe2 exd4 26. Bxc4 Rxe2 27. Bxd5+ Qxd5 28. Rxe2 dxc3 { White resigns. } 0-1',
+        '2025-04-24T16:02:54Z',
+        '{"TimeControl": "30+3"}',
+        3,
+        'zGsFNtCE',
+        'test2_lc_id',
+        'chess_com_user'
+    );
 
--- INSERT INTO pgn_games("moves", "date","metadata", "owner_id", "black_player_name", "white_player_name") VALUES
---     (
---         '1. d4 Nf6 2. c4 e6 3. Nf3 d5 4. g3 c6 5. Qc2 Bb4+ 6. Nbd2 O-O 7. Bg2 b6 8. O-O Ba6 9. b3 Nbd7 10. Bb2 Rc8 11. Rfd1 c5 12. a3 Bxd2 13. Nxd2 cxd4 14. Bxd4 e5 15. Bb2 dxc4 16. bxc4 Qe7 17. Qa4 Nc5 18. Qb4 Rfe8 19. Nf1 Bb7 20. Bh3 Rcd8 21. Ne3 Qc7 22. f3 a5 23. Rxd8 Rxd8 24. Qc3 Re8 25. Qc2 h6 26. Rd1 Bc6 27. Rd2 Ba4 28. Qc3 Bc6 29. Nd5 Bxd5 30. cxd5 Qd6 31. e4 Na4 32. Qc6 Qc5+ 33. Qxc5 Nxc5 34. d6 Nfd7 35. Rc2 f6 36. Bf1 Rd8 37. Bb5 Nf8 38. Rd2 Nfe6 39. a4 Nb3 40. Rd5 Nbd4 41. Bc4 Nxf3+ 42. Kf2 Nfg5 43. Ke3 Kf8 44. Ba3 Nc5 45. Bxc5 bxc5 46. Rxc5 Rxd6 47. h4 Ne6 48. Rxa5 Nc7 49. Rc5 Na6 50. Rc8+ Ke7 51. a5 Nb4 52. Rg8 g5 53. h5 Nc2+ 54. Kf2 Nb4 55. Rg7+ Kf8 56. Rb7 Nd3+ 57. Bxd3 Rxd3 58. a6 Rd2+ 59. Ke3 Ra2 60. a7 Ra3+ 61. Ke2 1-0',
---         '2025-04-28T19:39:01Z',
---         '{"Event": "World Championships 2025", "Site": "Krakow, Poland"}',
---         1,
---         'aaa',
---         'bbb'
---     );
+INSERT INTO pgn_games("moves", "date", "metadata", "owner_id", "black_player_name", "white_player_name") VALUES
+    (
+        '1. d4 Nf6 2. c4 e6 3. Nf3 d5 4. g3 c6 5. Qc2 Bb4+ 6. Nbd2 O-O 7. Bg2 b6 8. O-O Ba6 9. b3 Nbd7 10. Bb2 Rc8 11. Rfd1 c5 12. a3 Bxd2 13. Nxd2 cxd4 14. Bxd4 e5 15. Bb2 dxc4 16. bxc4 Qe7 17. Qa4 Nc5 18. Qb4 Rfe8 19. Nf1 Bb7 20. Bh3 Rcd8 21. Ne3 Qc7 22. f3 a5 23. Rxd8 Rxd8 24. Qc3 Re8 25. Qc2 h6 26. Rd1 Bc6 27. Rd2 Ba4 28. Qc3 Bc6 29. Nd5 Bxd5 30. cxd5 Qd6 31. e4 Na4 32. Qc6 Qc5+ 33. Qxc5 Nxc5 34. d6 Nfd7 35. Rc2 f6 36. Bf1 Rd8 37. Bb5 Nf8 38. Rd2 Nfe6 39. a4 Nb3 40. Rd5 Nbd4 41. Bc4 Nxf3+ 42. Kf2 Nfg5 43. Ke3 Kf8 44. Ba3 Nc5 45. Bxc5 bxc5 46. Rxc5 Rxd6 47. h4 Ne6 48. Rxa5 Nc7 49. Rc5 Na6 50. Rc8+ Ke7 51. a5 Nb4 52. Rg8 g5 53. h5 Nc2+ 54. Kf2 Nb4 55. Rg7+ Kf8 56. Rb7 Nd3+ 57. Bxd3 Rxd3 58. a6 Rd2+ 59. Ke3 Ra2 60. a7 Ra3+ 61. Ke2 1-0',
+        '2025-04-28T19:39:01Z',
+        '{"Event": "World Championships 2025", "Site": "Krakow, Poland"}',
+        1,
+        'aaa',
+        'bbb'
+    );
 
 INSERT INTO openings("eco", "name", "epd") VALUES
     (
