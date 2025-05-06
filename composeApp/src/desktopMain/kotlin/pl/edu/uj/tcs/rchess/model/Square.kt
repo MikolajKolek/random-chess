d99f29693c9beb3e0565ac17e7a36d301dd1b021
Djammnick <dzamnikplayer@gmail.com>
2025-05-06 22:02:03 +0200

    Removed needless variable self-assignment.
    Co-Authored-By: MikolajKolek <kolek.mikolaj@gmail.com>

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/Square.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/Square.kt
@@ -10,7 +10,5 @@ class Square(var row: Int, var col: Int, var piece: Piece? = null) {
     init {
         require(!(row < 0 || row > 7)) { "Row out of range." }
         require(!(col < 0 || col > 7)) { "Column out of range." }
-        this.row = row
-        this.col = col
     }
 }
