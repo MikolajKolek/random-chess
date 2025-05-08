ba84ab86ccc84259a96dd9b74eaa914459965485
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-05-08 18:23:03 +0200

    Scaffold sidebar

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,14 +8,15 @@ plugins {
 
 kotlin {
     jvm("desktop")
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
---
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/components/Board.kt
@@ -0,0 +1,11 @@
+package pl.edu.uj.tcs.rchess.components
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun Board() {
+
+}
+
