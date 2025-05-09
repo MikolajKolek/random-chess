51d3514067703b6c413eb39e7dfc30433d53b6c1
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-05-09 20:34:47 +0200

    Scaffold labels in Board.kt

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/components/Board.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/components/Board.kt
@@ -1,13 +1,13 @@
 package pl.edu.uj.tcs.rchess.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import pl.edu.uj.tcs.rchess.model.BoardState
 import pl.edu.uj.tcs.rchess.model.PlayerColor
@@ -16,45 +16,79 @@ import pl.edu.uj.tcs.rchess.model.Square
 @Composable
 @Preview
 fun Board(state: BoardState, playerColor: PlayerColor) {
-    val rows = when (playerColor) {
+    val ranks = when (playerColor) {
         PlayerColor.WHITE -> 7 downTo 0
         PlayerColor.BLACK -> 0..7
     }
-    val columns = when (playerColor) {
+    val files = when (playerColor) {
         PlayerColor.WHITE -> 0..7
         PlayerColor.BLACK -> 7 downTo 0
     }
 
-    Column(
-        modifier = Modifier.width(512.dp).height(512.dp).border(2.dp, Color.Black),
-        verticalArrangement = Arrangement.SpaceEvenly,
-    ) {
-        for (row in rows) {
-            Row(
-                Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-            ) {
-                for (col in columns) {
-                    val square = Square(rank = row, file = col)
-                    val piece = state.getPieceAt(square)
+    val labelsPadding = 32.dp
+    val pieceSize = 64.dp
 
-                    // TODO: Also replace with enum
+    @Composable
+    fun FileLabelRow() {
+        Row(
+            modifier = Modifier.padding(horizontal = labelsPadding).height(labelsPadding).width(8 * pieceSize + 2 * labelsPadding),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            for (file in files) {
+                Text(file.toString())
+            }
+        }
+    }
+
+    Box {
+        Column(
+            modifier = Modifier.width(8 * pieceSize + 2 * labelsPadding).height(8 * pieceSize + 2 * labelsPadding),
+        ) {
+            FileLabelRow()
+
+            for (rank in ranks) {
+                Row(
+                    Modifier.fillMaxWidth().height(pieceSize),
+                ) {
                     Box(
                         modifier = Modifier
-                            .weight(1f)
-                            .aspectRatio(1f)
-                            .let {
-                                if (square.isDark) {
-                                    it.background(Color.LightGray)
-                                } else {
-                                    it
-                                }
-                            }
+                            .width(labelsPadding)
+                            .fillMaxHeight()
                     ) {
-                        Text("$row $col: ${piece?.fenLetter ?: "-"}")
+                        Text(rank.toString())
+                    }
+
+                    for (file in files) {
+                        val square = Square(rank = rank, file = file)
+                        val piece = state.getPieceAt(square)
+
+                        Box(
+                            modifier = Modifier
+                                .width(pieceSize)
+                                .fillMaxHeight()
+                                .let {
+                                    if (square.isDark) {
+                                        it.background(Color.LightGray)
+                                    } else {
+                                        it
+                                    }
+                                }
+                        ) {
+                            Text("$rank $file: ${piece?.fenLetter ?: "-"}")
+                        }
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .width(labelsPadding)
+                            .fillMaxHeight()
+                    ) {
+                        Text(rank.toString())
                     }
                 }
             }
+
+            FileLabelRow()
         }
     }
 }
