279f1ad1bb5da9fb1a961abb2f84d560dafc84be
Djammnick <dzamnikplayer@gmail.com>
2025-05-10 14:16:14 +0200

    Created test file for BoardState.

---
+++ composeApp/src/desktopTest/kotlin/pl/edu/uj/tcs/rchess/model/BoardStateTest.kt
@@ -0,0 +1,17 @@
+package pl.edu.uj.tcs.rchess.model
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class BoardStateTest {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun emptyPositionTest() {
+        BoardState.empty()
+        Assert.assertThrows(IllegalArgumentException::class.java) { BoardState.empty().isLegal() }
+    }
+}
