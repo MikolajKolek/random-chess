738cfba4bcd8cd1be0a5efdd17b40cc62d4d85e8
Mikolaj Kolek <kolek.mikolaj@gmail.com>
2025-05-11 03:35:41 +0200

    Fix king

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/pieces/King.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/pieces/King.kt
@@ -5,8 +5,8 @@ import pl.edu.uj.tcs.rchess.model.Move
 import pl.edu.uj.tcs.rchess.model.PlayerColor
 import pl.edu.uj.tcs.rchess.model.Square
 
-class King(owner: PlayerColor): Piece(owner = owner) {
-    val vectors = listOf(
+class King(owner: PlayerColor): VectorListPiece(owner = owner) {
+    override val vectors = listOf(
         Square.Vector(1, 1),
         Square.Vector(1, 0),
         Square.Vector(1, -1),
@@ -18,27 +18,21 @@ class King(owner: PlayerColor): Piece(owner = owner) {
     )
 
     override fun getMoveVision(board: BoardState, square: Square): List<Move> {
-        val ret = mutableListOf<Move>()
-        for(v in vectors) {
-            if(board.getPieceAt(Square(square.rank+v.rank, square.file+v.file)) == null)
-                ret.add(Move(square, Square(square.rank+v.rank, square.file+v.file)))
-        }
-        if(owner == PlayerColor.WHITE) {
-            if(board.castlingRights.whiteKingSide) ret.add(Move(Square.fromString("e1"), Square.fromString("g1")))
-            if(board.castlingRights.whiteQueenSide) ret.add(Move(Square.fromString("e1"), Square.fromString("c1")))
-        } else {
-            if(board.castlingRights.blackKingSide) ret.add(Move(Square.fromString("e8"), Square.fromString("g8")))
-            if(board.castlingRights.blackQueenSide) ret.add(Move(Square.fromString("e8"), Square.fromString("c8")))
-        }
-        return ret
-    }
+        val ret = super.getMoveVision(board, square).toMutableList()
 
-    override fun getCaptureVision(board: BoardState, square: Square): List<Move> {
-        val ret = mutableListOf<Move>()
-        for(v in vectors) {
-            if(board.getPieceAt(Square(square.rank+v.rank, square.file+v.file)) != null)
-                ret.add(Move(square, Square(square.rank+v.rank, square.file+v.file)))
+        if(owner == PlayerColor.WHITE) {
+            if(board.castlingRights.whiteKingSide)
+                ret.add(Move(Square.fromString("e1"), Square.fromString("g1")))
+            if(board.castlingRights.whiteQueenSide)
+                ret.add(Move(Square.fromString("e1"), Square.fromString("c1")))
         }
+        else {
+            if(board.castlingRights.blackKingSide)
+                ret.add(Move(Square.fromString("e8"), Square.fromString("g8")))
+            if(board.castlingRights.blackQueenSide)
+                ret.add(Move(Square.fromString("e8"), Square.fromString("c8")))
+        }
+
         return ret
     }
 
