7e35a0a5d87432108520f9a00f702add7983a20b
Djammnick <dzamnikplayer@gmail.com>
2025-05-11 02:09:45 +0200

    Implemented castling in king moves.

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/pieces/King.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/model/pieces/King.kt
@@ -1,10 +1,12 @@
 package pl.edu.uj.tcs.rchess.model.pieces
 
+import pl.edu.uj.tcs.rchess.model.BoardState
+import pl.edu.uj.tcs.rchess.model.Move
 import pl.edu.uj.tcs.rchess.model.PlayerColor
 import pl.edu.uj.tcs.rchess.model.Square
 
-class King(owner: PlayerColor): VectorListPiece(owner = owner) {
-    override val vectors = listOf(
+class King(owner: PlayerColor): Piece(owner = owner) {
+    val vectors = listOf(
         Square.Vector(1, 1),
         Square.Vector(1, 0),
         Square.Vector(1, -1),
@@ -14,6 +16,31 @@ class King(owner: PlayerColor): VectorListPiece(owner = owner) {
         Square.Vector(-1, 0),
         Square.Vector(-1, -1),
     )
+
+    override fun getMoveVision(board: BoardState, square: Square): List<Move> {
+        val ret = mutableListOf<Move>()
+        for(v in vectors) {
+            if(board.getPieceAt(Square(square.rank+v.rank, square.file+v.file)) == null)
+                ret.add(Move(square, Square(square.rank+v.rank, square.file+v.file)))
+        }
+        if(owner == PlayerColor.WHITE) {
+            if(board.castlingRights.whiteKingSide) ret.add(Move(Square.fromString("e1"), Square.fromString("g1")))
+            if(board.castlingRights.whiteQueenSide) ret.add(Move(Square.fromString("e1"), Square.fromString("c1")))
+        } else {
+            if(board.castlingRights.blackKingSide) ret.add(Move(Square.fromString("e8"), Square.fromString("g8")))
+            if(board.castlingRights.blackQueenSide) ret.add(Move(Square.fromString("e8"), Square.fromString("c8")))
+        }
+        return ret
+    }
+
+    override fun getCaptureVision(board: BoardState, square: Square): List<Move> {
+        val ret = mutableListOf<Move>()
+        for(v in vectors) {
+            if(board.getPieceAt(Square(square.rank+v.rank, square.file+v.file)) != null)
+                ret.add(Move(square, Square(square.rank+v.rank, square.file+v.file)))
+        }
+        return ret
+    }
 
     override val fenLetterLowercase = 'k'
 }
