59538e0fa7e10b938df3ed58ce7ef6640dd33284
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-05-26 12:17:09 +0200

    Make placeholders more elegant

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/view/PlaceholderScreen.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/view/PlaceholderScreen.kt
@@ -1,6 +1,7 @@
 package pl.edu.uj.tcs.rchess.view
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,7 +14,10 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun PlaceholderScreen(modifier: Modifier = Modifier, text: String) {
     Box(
-        modifier = Modifier.padding(8.dp).then(modifier),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .then(modifier),
         contentAlignment = Alignment.Center,
     ) {
         Text(
