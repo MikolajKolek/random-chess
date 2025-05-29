307ae9a964982bafa8c34c8a4b5b4b8758545b04
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-05-29 15:52:34 +0200

    Fix live games not updating

--- composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/view/game/GameWindowContent.kt
+++ composeApp/src/desktopMain/kotlin/pl/edu/uj/tcs/rchess/view/game/GameWindowContent.kt
@@ -1,20 +1,22 @@
 package pl.edu.uj.tcs.rchess.view.game
 
 import androidx.compose.runtime.Composable
-import pl.edu.uj.tcs.rchess.model.state.GameState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import pl.edu.uj.tcs.rchess.server.game.ApiGame
 import pl.edu.uj.tcs.rchess.server.game.HistoryGame
 import pl.edu.uj.tcs.rchess.server.game.LiveGame
 import pl.edu.uj.tcs.rchess.view.theme.RandomChessTheme
-import kotlin.time.ExperimentalTime
 
-@OptIn(ExperimentalTime::class)
 @Composable
 fun GameWindowContent(game: ApiGame) {
     RandomChessTheme {
-        val gameState: GameState = when (game) {
-            is HistoryGame -> game.finalGameState
-            is LiveGame -> game.controls.observer.stateFlow.value
+        val gameState by when (game) {
+            is HistoryGame -> derivedStateOf { game.finalGameState }
+            is LiveGame -> {
+                game.controls.observer.stateFlow.collectAsStateWithLifecycle()
+            }
         }
         val input = (game as? LiveGame)?.controls?.input
 
