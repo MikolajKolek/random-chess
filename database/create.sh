8421407c3b9d9544e1cbb69a0aaf0e3bca62efe2
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-06-09 00:12:57 +0200

    Rename create.sql to schema-create.sql

--- database/create.sh
+++ database/create.sh
@@ -8,7 +8,7 @@ sudo -H -u postgres bash -c "\
   psql < $mydir/clear.sql
   psql -c \"CREATE USER random_chess WITH PASSWORD 'random_chess';\"
   psql -c \"CREATE DATABASE random_chess OWNER random_chess;\"
-  psql \"postgresql://random_chess:random_chess@localhost:5432/random_chess\" < $mydir/create.sql
+  psql \"postgresql://random_chess:random_chess@localhost:5432/random_chess\" < $mydir/schema-create.sql
   psql \"postgresql://random_chess:random_chess@localhost:5432/random_chess\" < $mydir/openings.sql
   psql \"postgresql://random_chess:random_chess@localhost:5432/random_chess\" < $mydir/example.sql
   if test -f $mydir/secret.sql; then
