1f447b96bab87d5313e79151411b4f98d998b1a1
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-06-13 14:46:01 +0200

    Update create.ps1 script

--- database/create.ps1
+++ database/create.ps1
@@ -1,21 +1,26 @@
-$ErrorActionPreference = "Stop"
+try {
+  $ErrorActionPreference = "Stop"
 
-psql -U postgres -f "clear.sql"
+  psql -U postgres -f "clear.sql"
 
-psql -U postgres -c "CREATE USER random_chess WITH PASSWORD 'random_chess';"
-psql -U postgres -c "CREATE DATABASE random_chess OWNER random_chess;"
+  psql -U postgres -c "CREATE USER random_chess WITH PASSWORD 'random_chess';"
+  psql -U postgres -c "CREATE DATABASE random_chess OWNER random_chess;"
 
-$env:PGPASSWORD = "random_chess"
+  $env:PGPASSWORD = "random_chess"
 
-psql -U random_chess -d random_chess -f "schema-create.sql"
-psql -U random_chess -d random_chess -f "schema-alteration.sql"
+  psql -U random_chess -d random_chess -f "schema-create.sql"
+  psql -U random_chess -d random_chess -f "schema-alteration.sql"
 
-if (Test-Path "openings.sql") {
+  if (Test-Path "openings.sql") {
     psql -U random_chess -d random_chess -f "openings.sql"
-}
+  }
 
-psql -U random_chess -d random_chess -f "example.sql"
+  psql -U random_chess -d random_chess -f "example.sql"
 
-if (Test-Path "secret.sql") {
+  if (Test-Path "secret.sql") {
     psql -U random_chess -d random_chess -f "secret.sql"
+  }
+}
+finally {
+    Remove-Item Env:PGPASSWORD
 }
