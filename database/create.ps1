07764b869b432484b7fff4baa47f58b9993bd6ca
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-06-13 14:38:07 +0200

    Add a PowerShell database init script

---
+++ database/create.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "Stop"
+
+psql -U postgres -f "clear.sql"
+
+psql -U postgres -c "CREATE USER random_chess WITH PASSWORD 'random_chess';"
+psql -U postgres -c "CREATE DATABASE random_chess OWNER random_chess;"
+
+$env:PGPASSWORD = "random_chess"
+
+psql -U random_chess -d random_chess -f "schema-create.sql"
+psql -U random_chess -d random_chess -f "schema-alteration.sql"
+
+if (Test-Path "openings.sql") {
+    psql -U random_chess -d random_chess -f "openings.sql"
+}
+
+psql -U random_chess -d random_chess -f "example.sql"
+
+if (Test-Path "secret.sql") {
+    psql -U random_chess -d random_chess -f "secret.sql"
+}
